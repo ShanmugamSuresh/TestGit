938bfca0b5fab7272987f3089410a5ea4c4ebaa8
venkatreddyk <suresh.shanmugam@capgemini.com>
2017-03-24 16:36:45 +0530

    .Text.txt.swp file is deleted and Test1.php file is added

---
+++ Test1.php
@@ -0,0 +1,10 @@
+This is my first commit. This is my first commit.This is my first commit.This is my first commit.This is my first commit.This is my first commit.
+This is my first commit. This is my first commit.This is my first commit.This is my first commit.This is my first commit.This is my first commit.
+This is my first commit. This is my first commit.This is my first commit.This is my first commit.This is my first commit.This is my first commit.
+This is my first commit. This is my first commit.This is my first commit.This is my first commit.This is my first commit.This is my first commit.
+
+Edit the text content.
+
+New branch is created.
+
+Update Text file.
